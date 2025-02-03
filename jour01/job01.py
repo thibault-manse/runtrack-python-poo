3d7cea390c366b8e01c0687195135b5d742a0c17
thibault-manse <thibault.manse@laplateforme.io>
2025-02-03 15:38:58 +0100

    add - job01 clear

---
+++ jour01/job01.py
@@ -0,0 +1,7 @@
+class operation :
+    def __init__(self, nombre1=15, nombre2=5):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+une_operation = operation()
+print(une_operation)
